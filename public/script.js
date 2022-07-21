9f3b5f4c4ba6e15a0d07f0fdff7923246f05ace4
hcweigand10 <henryweigand10@gmail.com>
2022-07-21 13:47:02 -0700

    rendering

--- public/script.js
+++ public/script.js
@@ -2,7 +2,34 @@ const veggiesList = document.getElementById("veggies")
 
 const init = async () => {
     const dataRaw = await fetch("/api/veggies")
-    console.log(dataRaw)
+    const data = await dataRaw.json()
+    console.log(data)
+    renderPage(data)
 }
 
-init()
+const init2 = () => {
+    fetch("/api/veggies")
+    .then(data => data.json())
+    .then(data => {
+        console.log(data)
+        renderPage(data)
+    })
+}
+
+const renderPage = (data) => {
+    data.forEach(element => {
+        const listItem = document.createElement("li")
+        const card = document.createElement("div")
+        const title = document.createElement("h4")
+        title.textContent = element.name
+        const color = document.createElement("p")
+        color.textContent = element.color
+        const size = document.createElement("p")
+        size.textContent = element.size
+        card.append(title,color,size)
+        listItem.append(card)
+        veggiesList.append(listItem)
+    });
+}
+
+init2()
