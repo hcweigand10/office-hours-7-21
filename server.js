3340993ccb255881fb12e2488a41f947634353c1
hcweigand10 <henryweigand10@gmail.com>
2022-07-21 12:20:25 -0700

    api server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const PORT = 3002
+const veggies = require("./veggies.json")
+
+const app = express()
+
+app.get("/hi", (req,res) => {
+    return res.json({msg: "hi"})
+})
+
+app.get("/api/veggies", (req, res) => {
+    return res.json(veggies)
+})
+
+app.listen(PORT, () => {
+    console.log(`API server is live at http://localhost:${PORT}`)
+})
